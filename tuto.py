84f7605c4600e5976c86fa7e6aabf091359c7eb1
shin77 <ishiyama.shin698@mail.kyutech,jp>
2021-10-12 22:56:49 +0900

    tanomimasu

---
+++ tuto.py
@@ -0,0 +1,103 @@
+import cgi
+
+	
+# cgiはcgiプログラムに使うモジュールだ。
+
+	
+import cgitb
+
+	
+# cgitbはcgiプログラムデバッグに関するモジュールだ。(エラーが発生すればphpみたいにエラーを画面に表示する。)
+
+	
+cgitb.enable()
+
+	
+ 
+
+	
+# パラメータを取得するための関数
+
+	
+# get、post区分なしでデータを持ち込む。
+
+	
+form = cgi.FieldStorage()
+
+	
+# パラメータdataを取得する。
+
+	
+data = form.getvalue('data')
+
+	
+# パラメータのtestを取得する。
+
+	
+test = form.getvalue('test')
+
+	
+# 画面い応答するhtmlドキュメント
+
+	
+html = f"""	
+
+	
+<!DOCTYPE html>	
+
+	
+<html>	
+
+	
+  <head><title>python</title></head>	
+
+	
+  <body>	
+
+	
+    <form method='post'>	
+
+	
+      <input type='text' name='test' value='hello'>	
+
+	
+      <input type='submit'>	
+
+	
+    </form>	
+
+	
+    <br />	
+
+	
+    data - {data} <br />	
+
+	
+    test - {test} <br />	
+
+	
+  </body>	
+
+	
+</html>	
+
+	
+""";	
+
+	
+# ヘッダータイプ設定
+
+	
+print("Content-type:text/html")	
+
+	
+# httpプロトコールでheaderとbodyの区分は改行なので必ず入れる。なければエラーに発生する。(bodyがないhttpファイルなので)
+
+	
+print('')
+
+	
+# バーディーを出力
+
+	
+print(html)
