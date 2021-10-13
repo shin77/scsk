c989378714e8e57893142721513c084150771403
shin77 <ishiyama.shin698@mail.kyutech,jp>
2021-10-13 11:58:35 +0900

    html

--- tuto.py
+++ tuto.py
@@ -1,45 +1,17 @@
 import cgi
-
-	
 # cgiはcgiプログラムに使うモジュールだ。
-
-	
 import cgitb
-
-	
 # cgitbはcgiプログラムデバッグに関するモジュールだ。(エラーが発生すればphpみたいにエラーを画面に表示する。)
-
-	
 cgitb.enable()
-
-	
- 
-
-	
 # パラメータを取得するための関数
-
-	
 # get、post区分なしでデータを持ち込む。
-
-	
 form = cgi.FieldStorage()
-
-	
 # パラメータdataを取得する。
-
-	
 data = form.getvalue('data')
-
-	
 # パラメータのtestを取得する。
-
-	
-test = form.getvalue('test')
-
-	
+test = form.getvalue('test')	
 # 画面い応答するhtmlドキュメント
 
-	
 html = f"""	
 
 	
